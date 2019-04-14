2beef972221339aaf7fb6505f23e1566a67fd6f8
iloveyougit <ankith.bhat.india@gmail.com>
2019-04-14 22:02:00 +0530

    3 commit

--- model/load.py
+++ model/load.py
@@ -1,7 +1,7 @@
 import numpy as np
 import keras.models
 from keras.models import model_from_json
-from scipy.misc import imread, imresize,imshow
+#from scipy.misc import imread, imresize,imshow
 import tensorflow as tf
 
 
